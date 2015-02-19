4fc252b47cf5631a6f6acc36eaf48560cd3974cd
fneyron <florent.neyron@gmail.com>
2015-02-19 14:58:52 +0100

    Renaming master file

--- clisphere.ps1
+++ clisphere.ps1
@@ -1,115 +1,119 @@
-#variables
-$ScriptRoot = Split-Path $MyInvocation.MyCommand.Path
-$csvfile = "$ScriptRoot\vmsdeploy.csv"
-$vcenter_srv = 'pcc-37-187-228-26.ovh.com'
-$timeout = 1800
+$vcserver = "pcc-37-187-228-26.ovh.com"
+$csvfile = "vmsdeploy.csv"
+$timeout = 240
 $loop_control = 0
 
-$vmsnapin = Get-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue
-$Error.Clear()
-if ($vmsnapin -eq $null) 	
-	{
-	Add-PSSnapin VMware.VimAutomation.Core
-	if ($error.Count -eq 0)
-		{
-		write-host "PowerCLI VimAutomation.Core Snap-in was successfully enabled." -ForegroundColor Green
-		}
-	else
-		{
-		write-host "ERROR: Could not enable PowerCLI VimAutomation.Core Snap-in, exiting script" -ForegroundColor Red
-		Exit
-		}
-	}
-else
-	{
-	Write-Host "PowerCLI VimAutomation.Core Snap-in is already enabled" -ForegroundColor Green
-	}
-
-# 32-bit PowerCLI is required to run *-OSCustomizationSpec cmdlets
-if ($env:Processor_Architecture -eq "x86") {
-
-	#Connect to vCenter
-	Connect-VIServer -Server $vcenter_srv
-
-	$vms2deploy = Import-Csv -Path $csvfile
-	
-	#deploy vms as per information in each line, wait for customization (a reboot) then wait for vmware tools to change ip settings
-	foreach ($vm in $vms2deploy) {
-		
-        #validate input, at least vm name, template name and OS Customization Spec name should be provided
-		if (($vm.name -ne "") -and ($vm.template -ne "") -and ($vm.oscust -ne "")){
-			
-			#check if vm with this name already exists (some funny results are produced once we deploy vm with duplicate name)
-			if (!(get-vm $vm.name -erroraction 0)){
-				$vmhost = get-cluster $vm.cluster | get-vmhost -state connected | Get-Random
-    			
-                #check if we want to attempt IP configuration, if "none" is written in .csv file (because we use DHCP for example) we deploy immediately, otherwise we insert IP into CustomizationSpec, then deploy
-				if ($vm.ip -match ‘none’){
-					Write-Host "No IP configuration in .csv file, moving on" -ForegroundColor Yellow
-                    write-Host "Deploying VM $($vm.name) to datastore cluster $($vm.datastore-cluster)"
-	   			    new-vm -name $vm.name -template $(get-template -name $vm.template) -vmhost $vmhost -oscustomizationspec $(get-oscustomizationspec -name $vm.oscust) -datastore $(get-datastore -name $vm.datastore) -location $(get-folder -name $vm.folder) | Out-Null
-				}
-				else {
-                    #clone the "master" OS Customization Spec, then use it to apply vm specific IP configuration
-					$cloned_oscust = Get-OSCustomizationSpec $vm.oscust | New-OSCustomizationSpec -name "$($vm.oscust)_$($vm.name)"
-					 
-					Set-OSCustomizationNicMapping -OSCustomizationNicMapping ($cloned_oscust | Get-OscustomizationNicMapping) -Position 1 -IpMode UseStaticIp -IpAddress $vm.ip -SubnetMask $vm.mask -DefaultGateway $vm.gw -Dns $vm.dns1,$vm.dns2 | Out-Null
-                    write-Host "Deploying VM $($vm.name) to datastore cluster $($vm.datastore)"
-	   			    new-vm -name $vm.name -template $(get-template -name $vm.template) -vmhost $vmhost -oscustomizationspec $cloned_oscust -datastore $(get-datastorecluster -name $vm.datastore-cluster) -location $(get-folder -name $vm.folder) | Out-Null
-				}
-				
-                #this is where we try to track deployment progress
-                $loop_control = 0
-				write-host "Starting VM $($vm.name)"
-    			start-vm -vm $vm.name -confirm:$false | Out-Null
-
-				write-host "Waiting for first boot of $($vm.name)" -ForegroundColor Yellow
-	    		do {
-    	    		$toolsStatus = (Get-VM -name $vm.name).extensiondata.Guest.ToolsStatus
-        			Start-Sleep 3
-					$loop_control++
-    			} until ( ($toolsStatus -match ‘toolsOk’) -or ($loop_control -gt $timeout) )
-
-				write-host "Waiting for customization spec to apply for $($vm.name) (a reboot)" -ForegroundColor Green
-    			do {
-        			$toolsStatus = (Get-VM -name $vm.name).extensiondata.Guest.ToolsStatus
-        			Start-Sleep 3
-					$loop_control++
-    			} until ( ($toolsStatus -match ‘toolsNotRunning’) -or ($loop_control -gt $timeout) )
-
-				Write-Host "OS customization in progress for $($vm.name)" -ForegroundColor red
-	    		do {
-    	    		$toolsStatus = (Get-VM -name $vm.name).extensiondata.Guest.ToolsStatus
-        			Start-Sleep 3
-                                $loop_control++
-    			} until ( ($toolsStatus -match ‘toolsOk’) -or ($loop_control -gt $timeout) )
-
-				#wait another minute "just in case" feel free to remove this line
-				Start-Sleep 60
-			    
-                #clean-up the cloned OS Customization spec
-				Remove-OSCustomizationSpec -CustomizationSpec $cloned_oscust -Confirm:$false | Out-Null
-				
-				if ($loop_control -gt $timeout){
-					Write-Host "Deployment of $($vm.name) took more than $($timeout/20) minutes, check if everything OK" -ForegroundColor red
-				}
-				else {
-				Write-Host "$($vm.name) successfully deployed, moving on" -ForegroundColor Green
-				}
-			}
-			else {
-				Write-Host "$($vm.name) already exists, moving on" -ForegroundColor Red
-			}
-		}
-		else {
-			Write-Host "Check input file $csvfile for line with empty vm, template or OS customization spec value" -ForegroundColor Red
-		}
-	}
-
-	Write-Host "All vms deployed, exiting" -ForegroundColor Green
-	#disconnect vCenter
-	Disconnect-VIServer -Confirm:$false
+function GetItemIdFromTitle([string]$LookupTitle, [ref]$LookupId)
+{   
+    $LookupField = $LookupList.Fields["DEPTCATEGORY"]   
+    $LookupItem = $LookupList.Items | where {$_['DEPTCATEGORY'] -like "*$LookupTitle*"} 
+    $LookupId.Value = $LookupItem.ID
 }
-else {
-Write-Host "This script should be run from 32-bit version of PowerCLI only, Open 32-bit PowerCLI window and start again" -ForegroundColor Red
+
+Function checkCSV($vm)
+{
+    $required = ($vm.Name, $vm.ressourcepool, $vm.Datastore, $vm.Template, $vm.Folder, $vm.Cluster, $vm.Custom)
+    foreach ($item in $required){
+        if ($item -eq $null) { 
+            Write-Host "Empty required value in CSV file. Exiting" -Foregroundcolor Red
+            Exit
+        }
+    }
+}
+
+# check vSphere snap in is loaded and connected to vcenter
+if(!$global:DefaultVIServer) {
+	if(!(Get-PSSnapin | Where {$_.name -eq "vmware.vimautomation.core"})) {
+		try {
+			$now = Get-Date
+			Write-Host "Snapin was not loaded, loading snapin: $now"
+			Add-PSSnapin VMware.VimAutomation.Core -ea 0| out-null
+		}
+		catch {
+			throw "Could not load PowerCLI snapin"
+            Exit
+		}
+	}
+	try {
+		$now = Get-Date
+		Write-Host "Not connected to VC server, connecting: $now"
+		Connect-VIServer $vcserver
+	}
+	catch {
+		throw "Failed to connect to VI server $vcserver"
+        Exit
+	}
+}
+
+#$GuestCredential = $Host.UI.PromptForCredential("Please enter credentials", "Enter Guest credentials for Template", "", "")
+$ScriptRoot = Split-Path $MyInvocation.MyCommand.Path
+
+$csvfile = "$ScriptRoot\$csvfile"
+if (!(Test-Path $csvfile)){
+    Write-Host "No csv file present $csvfile" -Foregroundcolor Red
+    Exit
+}
+$vms2deploy = Import-Csv -Path $csvfile
+foreach ($vm in $vms2deploy) {
+    # Check parameter are not empty in csv file
+    checkCSV($vm)
+    # Check vm Exist 
+    $vm_exist = Get-VM -Name $vm.name -ErrorAction SilentlyContinue
+    if (!($vm_exist)){
+        # Put VM on a random host in the cluster
+        $vmhost = Get-Cluster $vm.cluster | Get-VMHost -state connected | Get-Random
+        # If nothing configure in ip then use customization spec 
+        if (($vm.IP -eq $Null) -or ($vm.Netmask -eq $Null) -or ($vm.Gateway -eq $Null)){
+            
+            Write-Host "No IP configuration in .csv file, moving on" -ForegroundColor Yellow
+            write-Host "Deploying VM $($vm.name) to datastore cluster $($vm.datastore)"
+            new-vm -name $vm.name -template $(get-template -name $vm.template) -vmhost $vmhost -oscustomizationspec $(get-oscustomizationspec -name $vm.Custom) -datastore $(get-datastore -name $vm.datastore) -location $(get-folder -name $vm.folder) | Out-Null
+        }
+        else {
+            # clone the "master" OS Customization Spec, then use it to apply vm specific IP configuration
+            if (Get-OSCustomizationSpec "$($vm.Custom)_$($vm.Name)" -ErrorAction SilentlyContinue){
+                Remove-OSCustomizationSpec "$($vm.Custom)_$($vm.Name)" -Confirm:$false | Out-Null
+            }
+            $cloned_oscust = Get-OSCustomizationSpec $($vm.Custom) | New-OSCustomizationSpec -name "$($vm.Custom)_$($vm.Name)"
+            Set-OSCustomizationNicMapping -OSCustomizationNicMapping ($cloned_oscust | Get-OscustomizationNicMapping) -Position 1 -IpMode UseStaticIp -IpAddress $vm.IP -SubnetMask $vm.Netmask -DefaultGateway $vm.Gateway | Out-Null
+            write-Host "Deploying VM $($vm.name) to datastore cluster $($vm.datastore)"
+            new-vm -name $vm.Name -template $(get-template -name $vm.Template) -vmhost $vmhost -oscustomizationspec $cloned_oscust -Datastore $(get-datastore -name $vm.Datastore) -Location $(get-folder -name $vm.Folder) -ResourcePool $(get-resourcepool -name $vm.RessourcePool) | Out-Null
+        }
+
+        # Setting VLAN
+        Get-NetworkAdapter -vm $vm.Name | Set-NetworkAdapter -NetworkName $vm.VLAN -Confirm:$false
+
+        $loop_control = 0
+        write-host "Starting VM $($vm.name)"
+        start-vm -vm $vm.name -confirm:$false | Out-Null
+       
+       <# $command = "ifconfig eth0 down; ifconfig eth0 192.168.140.101; ifconfig eth0 up;"
+        Invoke-VMScript -VM CL02-SANDBOXFN-V001 -ScriptText $command -GuestUser $guestUser -GuestPassword VMware!#>
+
+        #clean-up the cloned OS Customization spec
+        Remove-OSCustomizationSpec -CustomizationSpec $cloned_oscust -Confirm:$false | Out-Null
+
+        # Wait until vm toosl start
+        write-host "Waiting for first boot of $($vm.name)" 
+        do {
+            $toolsStatus = (Get-VM -name $vm.name).extensiondata.Guest.ToolsStatus
+            Start-Sleep 1
+            $loop_control++
+        } until ( ($toolsStatus -eq "toolsOk") -or ($loop_control -gt $timeout) )
+        
+
+        Restart-vmGuest -vm $vm.Name
+       <# if ($loop_control -gt $timeout){
+            Write-Host "Deployment of $($vm.name) took more than $($timeout/60) minutes, check if everything OK" -ForegroundColor red
+        }
+        else {
+        Write-Host "$($vm.name) successfully deployed, moving on" -ForegroundColor Green
+        }#>
+    }
+    else {
+        Write-Host "$($vm.name) already exists, moving on" -ForegroundColor Red
+    }
+    Write-Host "All vms deployed, exiting" -ForegroundColor Green
+    #disconnect vCenter
+    #Disconnect-VIServer -Confirm:$false
 }
