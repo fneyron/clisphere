0494743e794337e7879af0708c151d38ecc7cb64
fneyron <florent.neyron@gmail.com>
2015-02-19 18:19:02 +0100

    Adding delete vm and deploy new scripts

---
+++ DeleteVMs.ps1
@@ -0,0 +1,100 @@
+#DNS server
+$DNS = $true
+$DNSServer = "CL02-INFRA-V001"
+$ZoneName = "oscaroad.com"
+
+Function Connect($vcserver)
+{
+    Write-Host "---------------------------------------------------"
+    Write-Host "Connecting to $vcserver..."
+    Write-Host "---------------------------------------------------"
+    if(!$global:DefaultVIServer) {
+        if(!(Get-PSSnapin | Where {$_.name -eq "vmware.vimautomation.core"})) {
+            try {
+                $now = Get-Date
+                Write-Host "Snapin was not loaded, loading snapin: $now"
+                Add-PSSnapin VMware.VimAutomation.Core -ea 0| out-null
+            }
+            catch {
+                throw "Could not load PowerCLI snapin"
+                Exit
+            }
+        }
+        try {
+            $now = Get-Date
+            Write-Host "Not connected to VC server, connecting: $now"
+            Connect-VIServer $vcserver
+        }
+        catch {
+            throw "Failed to connect to VI server $vcserver"
+            Exit
+        }
+    }
+    Write-Host "Success"
+}
+
+Function Main
+{
+    #$GuestCredential = $Host.UI.PromptForCredential("Please enter credentials", "Enter Guest credentials for Template", "", "")
+    $csvfile = "$ScriptRoot\$csvfile"
+    if (!(Test-Path $csvfile)){
+        Write-Host "No csv file present $csvfile" -Foregroundcolor Red
+        Exit
+    }
+    $vms2deploy = Import-Csv -Path $csvfile
+    foreach ($vm in $vms2deploy) {
+        Write-Host "---------------------------------------------------"
+        Write-Host "Deleting VM $($vm.Name) ..."
+        Write-Host "---------------------------------------------------"
+        $VMExist = Get-VM -Name $vm.name -ErrorAction SilentlyContinue
+        if (!($VMExist))
+        {  
+            Write-Host "VM $($vm.Name) doesn't exist, Skipping" -ForegroundColor Red
+        }
+        else {
+            Stop-VM -VM (Get-VM -Name $vm.name) -Confirm:$false
+            Remove-VM $vm.name -DeleteFromDisk
+
+            
+            Write-Host "VM $($vm.Name) successfully deleted" -foregrouncolor Green
+        }
+        if ($DNS)
+        {
+            Write-Host "---------------------------------------------------"
+            Write-Host "Delete DNS Entries ..."
+            Write-Host "---------------------------------------------------"
+            echo $ZoneName
+            Invoke-Command -ComputerName $DNSServer -ScriptBlock {
+                Remove-DnsServerResourceRecord -ZoneName $ZoneName -Name $vm.name -RRType "A" -confirm:$false
+            }
+        }
+    }
+}
+
+
+Function checkCSV($vm)
+{
+    $required = ($vm.Name)
+    foreach ($item in $required){
+        if ($item -eq $null) { 
+            Write-Host "Empty required value in CSV file. Exiting" -Foregroundcolor Red
+            Exit
+        }
+    }
+}
+
+# Define script path
+$ScriptRoot = Split-Path $MyInvocation.MyCommand.Path
+
+# Check Arguements
+if ($args.Length -ne 2)
+{
+    Write-Host "Usage: clisphere.ps1 <server> <csv>"
+}
+else
+{
+    $server = $args[0]
+    $csvfile = $args[1]
+    Connect($server)
+    Main
+}
---
+++ DeployVMs.ps1
@@ -0,0 +1,199 @@
+# Developed by Florent NEYRON
+
+# Global variables
+# datastore search variables
+$datastoreMinimumSpaceGB = 10
+$datastoreMaximumVMs = 80
+$datastoreNameQuery = "pcc-000*"
+
+# PowerOn timeout
+$timeout = 60
+$loop_control = 0
+
+#DNS server
+$DNS = $false
+$DNSServer = "CL02-INFRA-V001"
+$ZoneName = "oscaroad.com"
+
+Function Connect($vcserver)
+{
+    Write-Host "---------------------------------------------------"
+    Write-Host "Connecting to $vcserver..."
+    Write-Host "---------------------------------------------------"
+    if(!$global:DefaultVIServer) {
+        if(!(Get-PSSnapin | Where {$_.name -eq "vmware.vimautomation.core"})) {
+            try {
+                $now = Get-Date
+                Write-Host "Snapin was not loaded, loading snapin: $now"
+                Add-PSSnapin VMware.VimAutomation.Core -ea 0| out-null
+            }
+            catch {
+                throw "Could not load PowerCLI snapin"
+                Exit
+            }
+        }
+        try {
+            $now = Get-Date
+            Write-Host "Not connected to VC server, connecting: $now"
+            Connect-VIServer $vcserver
+        }
+        catch {
+            throw "Failed to connect to VI server $vcserver"
+            Exit
+        }
+    }
+    Write-Host "Success"
+}
+
+Function FindDatastore($vm)
+{
+    Write-Host "---------------------------------------------------"
+    Write-Host "Finding best datastores..."
+    Write-Host "---------------------------------------------------"
+
+    # get datastores
+    $datastores = @(Get-Datastore | `
+        # filter to datastores with name like $datastoreNameQuery with at least $datastoreMinumumSpace free space
+        ? {($_.Name -like $datastoreNameQuery) -and ($_.FreeSpaceGB -ge $datastoreMinimumSpaceGB)} | `
+        # select relevant data and get number of powered on VMs
+        select name, freespacegb, @{N="NumberVMs";E={@($_ | Get-VM | where {$_.PowerState -eq "PoweredOn"}).Count}} | `
+        # filter to datastores with less than $datastoreMaximumVMs
+        ? {($_.NumberVMs -lt $datastoreMaximumVMs)} | `
+        # sort VMs by number VMs then FreeSpace
+        Sort @{expression="FreeSpaceGbNumberVMs";Ascending=$true})
+        # if at any point we run out of space throw an error and exit
+    if(!$datastores) {
+        Write-Host "Not enough datastore space to deploy $vmQuantity VMs, deploy an extra datastore and run the script again"
+        exit
+    }
+    # if there are stores available continue and deduct a VM from the best store
+    else {
+        Write-Host "Using $($datastores[0].name), $([Math]::Round($datastores[0].FreeSpaceGB))GB free, $($datastores[0].NumberVMs) vms"
+    }
+    return $($datastores[0].name)
+}
+
+function CreateVM($vm)
+{
+    # Put VM on a random host in the cluster
+    $vmhost = Get-Cluster $vm.cluster | Get-VMHost -state connected | Get-Random
+    # If nothing configure in ip then use customization spec 
+    if (($vm.IP -eq $Null) -or ($vm.Netmask -eq $Null) -or ($vm.Gateway -eq $Null)){
+        Write-Host "No IP configuration in .csv file, moving on using default customization $vm.custom" 
+        $oscust = Get-OsCustomizationSpec -Name $vm.Custom
+    }
+    else {
+        # clone the "master" OS Customization Spec, then use it to apply vm specific IP configuration
+        if (Get-OSCustomizationSpec "$($vm.Custom)_$($vm.Name)" -ErrorAction SilentlyContinue){
+            Remove-OSCustomizationSpec "$($vm.Custom)_$($vm.Name)" -Confirm:$false | Out-Null
+        }
+        $oscust = Get-OSCustomizationSpec $($vm.Custom) | New-OSCustomizationSpec -name "$($vm.Custom)_$($vm.Name)"
+        Set-OSCustomizationNicMapping -OSCustomizationNicMapping ($oscust | Get-OscustomizationNicMapping) -Position 1 -IpMode UseStaticIp -IpAddress $vm.IP -SubnetMask $vm.Netmask -DefaultGateway $vm.Gateway | Out-Null
+    }
+    if ([string]::IsNullOrEmpty($vm.datastore))
+    {
+        # Finding Datastore
+        $datastore = FindDatastore($vm)
+    }
+    else 
+    {
+        $datastore = $vm.Datastore
+    }
+   
+    # Creating vm
+    new-vm -name $vm.Name -template $(get-template -name $vm.Template) -vmhost $vmhost -oscustomizationspec $oscust -Datastore $(get-datastore -name $datastore) -Location $(get-folder -name $vm.Folder) -ResourcePool $(get-resourcepool -name $vm.RessourcePool) | Out-Null
+    #clean-up the cloned OS Customization spec
+    Remove-OSCustomizationSpec -CustomizationSpec $oscust -Confirm:$false | Out-Null
+}
+
+Function Main
+{
+    #$GuestCredential = $Host.UI.PromptForCredential("Please enter credentials", "Enter Guest credentials for Template", "", "")
+    $csvfile = "$ScriptRoot\$csvfile"
+    if (!(Test-Path $csvfile)){
+        Write-Host "No csv file present $csvfile" -Foregroundcolor Red
+        Exit
+    }
+    $vms2deploy = Import-Csv -Path $csvfile
+    foreach ($vm in $vms2deploy) {
+        # Check parameter are not empty in csv file
+        checkCSV($vm)
+        Write-Host "---------------------------------------------------"
+        Write-Host "Deploying VM $($vm.Name) ..."
+        Write-Host "---------------------------------------------------"
+        # Check vm Exist 
+        $VMExist = Get-VM -Name $vm.name -ErrorAction SilentlyContinue
+        if ($VMExist)
+        {  
+            Write-Host "VM $($vm.Name) already exist, Skipping" -ForegroundColor Red
+        }
+        else
+        {
+            # Create VM
+            CreateVM($vm)
+
+            # Setting VLAN
+            Get-NetworkAdapter -vm $vm.Name | Set-NetworkAdapter -NetworkName $vm.VLAN -Confirm:$false | Out-Null
+
+            $loop_control = 0
+            write-host "Starting VM $($vm.name)"
+            start-vm -vm $vm.name -confirm:$false | Out-Null
+           
+           <# $command = "ifconfig eth0 down; ifconfig eth0 192.168.140.101; ifconfig eth0 up;"
+            Invoke-VMScript -VM CL02-SANDBOXFN-V001 -ScriptText $command -GuestUser $guestUser -GuestPassword VMware!#>
+
+            # Wait until vm toosl start
+            write-host "Waiting for first boot of $($vm.name)" 
+            do {
+                $toolsStatus = (Get-VM -name $vm.name).extensiondata.Guest.ToolsStatus
+                Start-Sleep 1
+                $loop_control++
+            } until ( ($toolsStatus -eq "toolsOk") -or ($loop_control -gt $timeout) )
+
+            if ($loop_control -gt $timeout){
+                Write-Host "Deployment of $($vm.name) took more than $($timeout/60) minutes, check if everything OK" -ForegroundColor red
+            }
+
+            Restart-vmGuest -vm $vm.Name
+            
+            if (($vm.IP -ne $Null) -and ($DNS))
+            {
+                Write-Host "---------------------------------------------------"
+                Write-Host "Updating DNS Entries ..."
+                Write-Host "---------------------------------------------------"
+
+                Invoke-Command -ComputerName $DNSServer -ScriptBlock {Add-DnsServerResourceRecordA -ZoneName $ZoneName -Name $vm.name -IPv4Address $vm.IP -CreatePtr}
+            }
+        }
+    }
+    Write-Host "All process Successfull, exiting" -ForegroundColor Green
+    #disconnect vCenter
+    #Disconnect-VIServer -Confirm:$false
+}
+
+Function checkCSV($vm)
+{
+    $required = ($vm.Name, $vm.ressourcepool, $vm.Template, $vm.Folder, $vm.Cluster, $vm.Custom)
+    foreach ($item in $required){
+        if ([string]::IsNullOrEmpty($item)) { 
+            Write-Host "Empty required value in CSV file. Exiting" -Foregroundcolor Red
+            Exit
+        }
+    }
+}
+
+# Define script path
+$ScriptRoot = Split-Path $MyInvocation.MyCommand.Path
+
+# Check Arguements
+if ($args.Length -ne 2)
+{
+    Write-Host "Usage: clisphere.ps1 <server> <csv>"
+}
+else
+{
+    $server = $args[0]
+    $csvfile = $args[1]
+    Connect($server)
+    Main
+}
